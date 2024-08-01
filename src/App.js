982f46f7426f7ef9e57d826a45a2acff405b6a90
B owie <marketing@letscode.com.br>
2024-08-01 20:53:30 -0300

    Mensagem do commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Baner from './componentes/Banner/Banner';
 
 
 function App() {
   return (
-
-    
+    <Baner />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
