02025be1357e580690ea4c8ab20526289e1a7d85
B owie <marketing@letscode.com.br>
2024-08-01 21:20:46 -0300

    Mensagem do commit

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,7 +1,11 @@
+import './banner.css'
+
 function Banner(){
     return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O Baner Principal da Página do Organo" />
+        </header>
         
-        <img src="/imagens/banner.png" alt="O Baner Principal da Página do Organo" />
     )
 }
 
